dafdde8d83e2545bd4ef46482de2fa7f044a75b2
Sohaib <83075386+SohaibAlvi1986@users.noreply.github.com>
2021-04-28 12:36:27 +0200

    Add files via upload

---
+++ GildedRose.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    class GildedRose
+    {
+        #region Variables/Constants
+
+        private IList<Item> items;
+
+        private string strSulfuras = "Sulfuras, Hand of Ragnaros";
+
+        private string strAgedBrie = "Aged Brie";
+
+        private string strBackstagePasses = "Sulfuras, Hand of Ragnaros";
+
+        private string strConjured = "Conjured";
+
+        #endregion
+
+        #region Constructor
+        public GildedRose(IList<Item> items)
+        {
+            this.items = items;
+        }
+        #endregion
+
+        #region Methods
+        public void UpdateQuality()
+        {
+            foreach (Item item in this.items)
+            {
+                ItemCategory category = this.categorize(item);
+                category.updateOneItem(item);
+            }
+        }
+
+        private ItemCategory categorize(Item item)
+        {
+            if (item.Name.Equals(strSulfuras))
+            {
+                return new Legendary();
+            }
+
+            if (item.Name.Equals(strAgedBrie))
+            {
+                return new Cheese();
+            }
+
+            if (item.Name.Equals(strBackstagePasses))
+            {
+                return new BackstagePass();
+            }
+
+            if (item.Name.StartsWith(strConjured))
+            {
+                return new Conjured();
+            }
+
+            return new ItemCategory();
+        }
+
+        #endregion
+
+        #region Public/Private Classes
+
+        private class ItemCategory
+        {
+
+            protected void incrementQuality(Item item)
+            {
+                if ((item.Quality < 50))
+                {
+                    item.Quality = (item.Quality + 1);
+                }
+
+            }
+
+            protected void decrementQuality(Item item)
+            {
+                if ((item.Quality > 0))
+                {
+                    item.Quality = (item.Quality - 1);
+                }
+
+            }
+
+            protected void updateExpired(Item item)
+            {
+                this.decrementQuality(item);
+            }
+
+            protected void updateSellIn(Item item)
+            {
+                item.SellIn = (item.SellIn - 1);
+            }
+
+            protected void updateQuality(Item item)
+            {
+                this.decrementQuality(item);
+            }
+
+            public void updateOneItem(Item item)
+            {
+                this.updateQuality(item);
+                this.updateSellIn(item);
+                if ((item.SellIn < 0))
+                {
+                    this.updateExpired(item);
+                }
+
+            }
+        }
+
+        private class Legendary : ItemCategory
+        {
+
+            protected void updateExpired(Item item)
+            {
+
+            }
+
+            protected void updateSellIn(Item item)
+            {
+
+            }
+
+            protected void updateQuality(Item item)
+            {
+
+            }
+        }
+
+        private class Cheese : ItemCategory
+        {
+
+            protected void updateExpired(Item item)
+            {
+                incrementQuality(item);
+            }
+
+            protected void updateQuality(Item item)
+            {
+                incrementQuality(item);
+            }
+        }
+
+        private class BackstagePass : ItemCategory
+        {
+
+            protected void updateExpired(Item item)
+            {
+                item.Quality = 0;
+            }
+
+            protected void updateQuality(Item item)
+            {
+                incrementQuality(item);
+                if ((item.SellIn <= 10))
+                {
+                    incrementQuality(item);
+                }
+
+                if ((item.SellIn <= 5))
+                {
+                    incrementQuality(item);
+                }
+
+            }
+        }
+
+        private class Conjured : ItemCategory
+        {
+
+            protected void updateExpired(Item item)
+            {
+                decrementQuality(item);
+                decrementQuality(item);
+            }
+
+            protected void updateQuality(Item item)
+            {
+                decrementQuality(item);
+                decrementQuality(item);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+Compile : 
+Download Code and open csharp.sln file in visual studio 2019, press Ctr+Shit+B for Build.
+
+Run :
+To Run the code after compilation , press Ctr + F5
+
